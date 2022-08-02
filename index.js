7616d26c63a04dbdcb8816ae43c16865aecd89e4
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2022-08-01 21:21:16 -0700

    write out some ideas

--- index.js
+++ index.js
@@ -1,61 +1,29 @@
-// VILLAGER
-// const example = {
-//   VillagerData: { profession: farmer, level: 2, type: plains },
-//   Invulnerable: 1,
-//   PersistenceRequired: 1,
-//   CustomName: '"Test"',
-//   Offers: {
-//     Recipes: [
-//       {
-//         buy: { id: emerald, Count: 5 },
-//         sell: { id: diamond, Count: 1 },
-//         xp: 10,
-//         maxUses: 9999999,
-//       },
-//     ],
-//   },
-// };
+const items = require("items");
 
-// DIRTY = CLEAN
-// const fs = require("fs");
-// const dirty = require("./dirty-item-groups.json");
-// let clean = [];
-// for (const e of dirty) {
-//   const name = e.name.split("(").pop().split(")")[0];
-//   const category = e.category;
-//   const stack_count = e.stack_count;
-//   clean.push({ name, category, stack_count });
-// }
-// fs.writeFileSync("clean-item-groups.json", JSON.stringify(clean));
+const baseUnitPrice = 1; // the lowest possible price to buy anything
+const currency = "emerald"; // what villagers will accept as currency
 
-// FILTER
-// const fs = require("fs");
-// const unfiltered = require("./clean-item-groups.json");
-// let cnt = 0;
-// let filtered = {};
-// for (const e of unfiltered) {
-//   cnt++;
-//   if (!filtered[e.category]) {
-//     filtered[e.category] = {};
-//   }
-//   filtered[e.category][e.name] = {
-//     category: e.category,
-//     stack_count: e.stack_count,
-//   };
-// }
-// console.log("items parsed:", cnt);
-// fs.writeFileSync("filtered-item-groups.json", JSON.stringify(filtered));
+// boilerplate for VillagerData
+const example = {
+  VillagerData: { profession: farmer, level: 2, type: plains },
+  Invulnerable: 1,
+  PersistenceRequired: 1,
+  CustomName: '"Test"',
+  Offers: {
+    Recipes: [
+      // {
+      //   buy: { id: emerald, Count: 5 },
+      //   sell: { id: diamond, Count: 1 },
+      //   xp: 10,
+      //   maxUses: 9999999,
+      // },
+    ],
+  },
+};
 
-// CHECK
-// const items = require("./filtered-item-groups.json");
-// let i = 0;
-// for (const k in items) {
-//   if (Object.hasOwnProperty.call(items, k)) {
-//     const e = items[k];
-//     const categoryItemCount = Object.entries(e).length;
-//     console.log(categoryItemCount);
-//     console.log(e);
-//     i += categoryItemCount;
-//   }
-// }
-// console.log(i);
+// CLI or website to define prices? (probably a website)
+// serve categories to user, prompting an item price and a resulting count
+// skip items which have not been specified to be sold at a specific price
+// add search functionality?
+// make currency default, but can be changed for specific items?
+// lots of ideas!
